e72461b1f4daf9003eec770badf4bc8c015f2f02
Romain Menke <romainmenke@gmail.com>
2026-02-01 10:51:04 +0100

    fmt

--- .tests/test-gs-uploads-stream-wrapper.php
+++ .tests/test-gs-uploads-stream-wrapper.php
@@ -11,7 +11,7 @@ class Test_GS_Uploads_Stream_Wrapper extends WP_UnitTestCase {
 	}
 
 	public function test_stream_wrapper_is_registered() {
-		$this->assertTrue( in_array( 'gs', stream_get_wrappers() ) );
+		$this->assertTrue( in_array( 'gs', stream_get_wrappers(), true ) );
 	}
 
 	public function test_copy_via_stream_wrapper() {
@@ -28,7 +28,7 @@ class Test_GS_Uploads_Stream_Wrapper extends WP_UnitTestCase {
 
 		$local_path  = __DIR__ . '/data/canola.jpg';
 		$remote_path = 'gs://not-a-bucket-c1dd5b42-45be-4a85-b82f-e0b0815d69e5/canola.jpg';
-		$result      = @copy( $local_path, $remote_path );
+		$result      = @copy( $local_path, $remote_path ); // phpcs:ignore
 
 		$this->assertFalse( $result );
 	}
@@ -36,7 +36,7 @@ class Test_GS_Uploads_Stream_Wrapper extends WP_UnitTestCase {
 	public function test_rename_via_stream_wrapper() {
 
 		copy( __DIR__ . '/data/canola.jpg', 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg' );
-		$result = rename( 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg', 'gs://' . GS_UPLOADS_BUCKET . '/canola-test.jpg' );
+		$result = rename( 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg', 'gs://' . GS_UPLOADS_BUCKET . '/canola-test.jpg' ); // phpcs:ignore
 		$this->assertTrue( $result );
 		$this->assertTrue( file_exists( 'gs://' . GS_UPLOADS_BUCKET . '/canola-test.jpg' ) );
 	}
@@ -55,7 +55,7 @@ class Test_GS_Uploads_Stream_Wrapper extends WP_UnitTestCase {
 	public function test_unlink_via_stream_wrapper() {
 
 		copy( __DIR__ . '/data/canola.jpg', 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg' );
-		$result = unlink( 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg' );
+		$result = unlink( 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg' ); // phpcs:ignore
 		$this->assertTrue( $result );
 		$this->assertFalse( file_exists( 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg' ) );
 	}
@@ -93,10 +93,12 @@ class Test_GS_Uploads_Stream_Wrapper extends WP_UnitTestCase {
 		// $file = 'gs://' . GS_UPLOADS_BUCKET . '/canola.jpg';
 		// copy( __DIR__ . '/data/canola.jpg', $file );
 
+		// phpcs:ignore-next-line
 		// $f      = fopen( $file, 'r' );
 		// $result = fseek( $f, 0, SEEK_END );
 		// fclose( $f );
 
+		// phpcs:ignore-next-line
 		// $this->assertEquals( 0, $result );
 
 		$this->assertTrue( true );
